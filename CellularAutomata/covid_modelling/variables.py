8a1ce58c8c62badbf37a5acccf8b432d571cc847
Fredrik Pedersen <fredrikhp@gmail.com>
2020-10-27 14:29:03 +0100

    Added PERCENTAGE_SOCIAL_DISTANCING and MANDATORY_ISOLATION variables. Fine tuned other variables to prevent too many runs of the adjustments.

--- CellularAutomata/covid_modelling/variables.py
+++ CellularAutomata/covid_modelling/variables.py
@@ -1,8 +1,11 @@
 from covid_modelling.constants import DAYS_SINCE_OUTBREAK
 
 STEP_LIMIT: int = DAYS_SINCE_OUTBREAK  # Limit number of steps the simulation takes before evolving.
+ADJUSTMENTS_COMPLETE: bool = False
 
-INFECTION_CHANCE: float = 0.3  # Percentage chance for a person to get infected when in contact with a sick person.
-MORTALITY_CHANCE: float = 0.05  # Modifier for checking if a person in a risk group will die from the virus
+INFECTION_CHANCE: float = 0.2  # Percentage chance for a person to get infected when in contact with a sick person.
+MORTALITY_CHANCE: float = 0.03  # Modifier for checking if a person in a risk group will die from the virus
 RECOVERY_CHANCE: float = 0.2  # Arbitrary number used for deciding if a person becomes healthy
 PERCENTAGE_USING_MASKS: float = 0.3  # Percentage using masks. This is a made up number.
+PERCENTAGE_SOCIAL_DISTANCING: float = 0.1  # Percentage practicing social distance. This is a made up number.
+MANDATORY_ISOLATION: bool = False
