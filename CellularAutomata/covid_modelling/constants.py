a26b369c1037b6c0120af3c06ea2fde468977f3e
Fredrik Pedersen <fredrikhp@gmail.com>
2020-10-30 16:45:29 +0100

    Changed constant back to real value

--- CellularAutomata/covid_modelling/constants.py
+++ CellularAutomata/covid_modelling/constants.py
@@ -10,7 +10,7 @@ SYMPTOMS: str = "Symptoms"
 
 # Simulation constants
 AREA_DIMENSIONS: int = 100  # The simulation has nxn number of cells. AREA_DIMENSIONS = n.
-DAYS_SINCE_OUTBREAK: int = 10  # We consider march 2nd as the pandemic start in Norway. Data was pulled 27.10.2020.
+DAYS_SINCE_OUTBREAK: int = 239  # We consider march 2nd as the pandemic start in Norway. Data was pulled 27.10.2020.
 INIT_INFECTION_PROBABILITY: float = 0.01  # Arbitrary number for setting number of initially infected when the simulation starts.
 
 # 27.10.2020 there have been 74.65 infections and 1.167 deaths on avergage per day. Doing it like this so I can run the simulation on shorter intervalls without it going completly bonkers.
