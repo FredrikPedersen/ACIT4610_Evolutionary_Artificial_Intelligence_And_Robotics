48bacdf3d7574760804d0d219aeeee2d8d3c3095
Fredrik Pedersen <fredrikhp@gmail.com>
2020-11-02 19:28:36 +0100

    Removed redundant frames, config and functions

--- CellularAutomata/PyCX/pycxsimulator.py
+++ CellularAutomata/PyCX/pycxsimulator.py
@@ -54,14 +54,7 @@ class GUI:
         self.notebook.pack(side=TOP, padx=2, pady=2)
 
         self.frameRun = Frame(self.rootWindow)
-        self.frameSettings = Frame(self.rootWindow)
-        self.frameParameters = Frame(self.rootWindow)
-        self.frameInformation = Frame(self.rootWindow)
-
         self.notebook.add(self.frameRun, text="Run")
-        self.notebook.add(self.frameSettings, text="Settings")
-        self.notebook.add(self.frameParameters, text="Parameters")
-        self.notebook.add(self.frameInformation, text="Info")
         self.notebook.pack(expand=NO, fill=BOTH, padx=5, pady=5, side=TOP)
 
         self.status = Label(self.rootWindow, width=40, height=3, relief=SUNKEN, bd=1, textvariable=self.statusText)
@@ -101,102 +94,11 @@ class GUI:
         self.buttonQuit.pack(side=TOP, padx=5, pady=5)
         self.showHelp(self.buttonQuit, "Exits the simulation")
 
-        # -----------------------------------
-        # frameSettings
-        # -----------------------------------
-        can = Canvas(self.frameSettings)
-
-        lab = Label(can, width=25, height=1, text="Step size ", justify=LEFT, anchor=W, takefocus=0)
-        lab.pack(side='left')
-
-        self.stepScale = Scale(can, from_=1, to=50, resolution=1, command=self.changeStepSize, orient=HORIZONTAL,
-                               width=25, length=150)
-        self.stepScale.set(self.stepSize)
-        self.showHelp(self.stepScale,
-                      "Skips model redraw during every [n] simulation steps\nResults in a faster model run.")
-        self.stepScale.pack(side='left')
-
-        can.pack(side='top')
-
-        can = Canvas(self.frameSettings)
-        lab = Label(can, width=25, height=1, text="Step visualization delay in ms ", justify=LEFT, anchor=W,
-                    takefocus=0)
-        lab.pack(side='left')
-        self.stepDelay = Scale(can, from_=0, to=max(2000, self.timeInterval),
-                               resolution=10, command=self.changeStepDelay, orient=HORIZONTAL, width=25, length=150)
-        self.stepDelay.set(self.timeInterval)
-        self.showHelp(self.stepDelay, "The visualization of each step is delays by the given number of milliseconds.")
-        self.stepDelay.pack(side='left')
-
-        can.pack(side='top')
-
-        # --------------------------------------------
-        # frameInformation
-        # --------------------------------------------
-        scrollInfo = Scrollbar(self.frameInformation)
-        self.textInformation = Text(self.frameInformation, width=45, height=13, bg='lightgray', wrap=WORD,
-                                    font=("Courier", 10))
-        scrollInfo.pack(side=RIGHT, fill=Y)
-        self.textInformation.pack(side=LEFT, fill=BOTH, expand=YES)
-        scrollInfo.config(command=self.textInformation.yview)
-        self.textInformation.config(yscrollcommand=scrollInfo.set)
-
-        # --------------------------------------------
-        # ParameterSetters
-        # --------------------------------------------
-        for variableSetter in self.parameterSetters:
-            can = Canvas(self.frameParameters)
-
-            lab = Label(can, width=25, height=1, text=variableSetter.__name__ + " ", anchor=W, takefocus=0)
-            lab.pack(side='left')
-
-            ent = Entry(can, width=11)
-            ent.insert(0, str(variableSetter()))
-
-            if variableSetter.__doc__ != None and len(variableSetter.__doc__) > 0:
-                self.showHelp(ent, variableSetter.__doc__.strip())
-
-            ent.pack(side='left')
-
-            can.pack(side='top')
-
-            self.varEntries[variableSetter] = ent
-
-        if len(self.parameterSetters) > 0:
-            self.buttonSaveParameters = Button(self.frameParameters, width=50, height=1,
-                                               command=self.saveParametersCmd,
-                                               text="Save parameters to the running model", state=DISABLED)
-            self.showHelp(self.buttonSaveParameters,
-                          "Saves the parameter values.\nNot all values may take effect on a running model\nA model reset might be required.")
-            self.buttonSaveParameters.pack(side='top', padx=5, pady=5)
-            self.buttonSaveParametersAndReset = Button(self.frameParameters, width=50, height=1,
-                                                       command=self.saveParametersAndResetCmd,
-                                                       text="Save parameters to the model and reset the model")
-            self.showHelp(self.buttonSaveParametersAndReset, "Saves the given parameter values and resets the model")
-            self.buttonSaveParametersAndReset.pack(side='top', padx=5, pady=5)
-
     # <<<<< Init >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     def setStatusStr(self, newStatus):
         self.statusStr = newStatus
         self.statusText.set(self.statusStr)
-
-        # model control functions for changing parameters
-
-    def changeStepSize(self, val):
-        self.stepSize = int(val)
-
-    def changeStepDelay(self, val):
-        self.timeInterval = int(val)
-
-    def saveParametersCmd(self):
-        for variableSetter in self.parameterSetters:
-            variableSetter(float(self.varEntries[variableSetter].get()))
-            self.setStatusStr("New parameter values have been set")
-
-    def saveParametersAndResetCmd(self):
-        self.saveParametersCmd()
-        self.resetModel(manualReset=False)
 
     # <<<< runEvent >>>>>
     # This event is envoked when "Run" button is clicked.
