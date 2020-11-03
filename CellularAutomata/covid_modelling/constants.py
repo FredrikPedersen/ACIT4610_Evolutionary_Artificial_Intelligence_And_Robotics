0566a1d711cb277202d0ba82ea1b265ee54056d8
Fredrik Pedersen <fredrikhp@gmail.com>
2020-11-03 19:24:23 +0100

    Updated constants based on new research findings

--- CellularAutomata/covid_modelling/constants.py
+++ CellularAutomata/covid_modelling/constants.py
@@ -1,6 +1,6 @@
 # Infection Data Constants
 AVERAGE_DURATION: int = 20  # On average, a person is recovered after 20 days of contracting the disease.
-MAX_DURATION: int = 42  # Six weeks are the longest recorded cases of people being sick
+MAX_DURATION: int = 61  # Six weeks are the longest recorded cases of people being sick
 INFECTIOUS_START: int = 3  # How many days after infection the disease becomes infectious
 INCUBATION_DURATION: int = 5  # How many days before the virus gives symptoms
 
@@ -21,7 +21,7 @@ MASK_MEASURE: str = "Wearing Mask"
 SOCIAL_DISTANCE_MEASURE: str = "Social Distancing"
 ISOLATION_MEASURE: str = "Isolation"
 
-MASK_REDUCTION: float = 0.35  # Approx risk reduction of using a mask
+MASK_REDUCTION: float = 0.65  # Approx risk reduction of using a mask
 DISTANCING_REDUCTION: float = 0.5  # Couldn't find any hard data on how much a 1m social distance reduces self risk of infection. Varies between 40 - 80% depending on source and age group.
 MORTAL_RISK_AGE: int = 65  # People aged over 65 are considered a risk group
-MORTAL_RISK_GROUP_PERCENTAGE: float = 0.34  # Approx 1.85mil out of 5.433mil are in the risk-group based on their health conditions. This is not including people aged > 65
+MORTAL_RISK_GROUP_PERCENTAGE: float = 0.35  # Approx 1.85mil out of 5.433mil are in the risk-group based on their health conditions. This is not including people aged > 65
