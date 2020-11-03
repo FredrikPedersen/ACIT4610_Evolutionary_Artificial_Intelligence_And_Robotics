32a2b46d1e8a24c2a4df6b23d1ff50200f8d1193
Fredrik Pedersen <fredrikhp@gmail.com>
2020-11-03 19:37:15 +0100

    Made MaskPercentage and DistancingPercentage graphs display total number of infected instead of fitness score as y-value.

--- CellularAutomata/covid_modelling/results/graph.py
+++ CellularAutomata/covid_modelling/results/graph.py
@@ -1,7 +1,6 @@
 from typing import List
 
 import matplotlib.pyplot as pyplot
-import covid_modelling.constants as constants
 
 from covid_modelling.results.simulation_run import SimulationRun
 
@@ -14,6 +13,8 @@ mask_values: str = "Mask Values"
 def draw_graphs(simulation_runs: List[SimulationRun]):
 
     __plot_benchmark_fitness_timestep(simulation_runs, [figure_with_isolation, figure_without_isolation])
+    del simulation_runs[0]  # Remove the benchmark run
+
     __draw_timestep_graphs(simulation_runs)
     __draw_mask_percentage_graphs(simulation_runs)
     __draw_distancing_percentage_graphs(simulation_runs)
@@ -24,7 +25,7 @@ def __draw_distancing_percentage_graphs(simulation_runs: List[SimulationRun]):
     without_isolation = []
 
     for simulation_run in simulation_runs:
-        if simulation_run.get_mandatory_isolation() is True:
+        if simulation_run.get_mandatory_isolation():
             with_isolation.append(simulation_run)
         else:
             without_isolation.append(simulation_run)
@@ -36,21 +37,21 @@ def __draw_distancing_percentage_graphs(simulation_runs: List[SimulationRun]):
     without_isolation_y: [float] = []
     for simulation_run in without_isolation:
         x.append(f"{simulation_run.get_percentage_social_distancing() * 100}%")
-        without_isolation_y.append(simulation_run.get_fitness_score()[constants.DAYS_SINCE_OUTBREAK])
+        without_isolation_y.append(simulation_run.get_infected())
 
     pyplot.figure(distancing_values)
     pyplot.plot(x, without_isolation_y, color="red", label="Without Isolation")
 
     with_isolation_y: [float] = []
     for simulation_run in with_isolation:
-        with_isolation_y.append(simulation_run.get_fitness_score()[constants.DAYS_SINCE_OUTBREAK])
+        with_isolation_y.append(simulation_run.get_infected())
 
     pyplot.figure(distancing_values)
     pyplot.plot(x, with_isolation_y, color="blue", label="With Isolation")
     pyplot.legend()
     pyplot.xlabel("Social Distancing Percentage")
-    pyplot.ylabel("Fitness Score")
-    pyplot.title("Distancing Fitness")
+    pyplot.ylabel("Number of Infected")
+    pyplot.title("Number of Infected Based on Social Distancing")
 
 
 def __draw_mask_percentage_graphs(simulation_runs: List[SimulationRun]):
@@ -58,7 +59,7 @@ def __draw_mask_percentage_graphs(simulation_runs: List[SimulationRun]):
     without_isolation = []
 
     for simulation_run in simulation_runs:
-        if simulation_run.get_mandatory_isolation() is True:
+        if simulation_run.get_mandatory_isolation():
             with_isolation.append(simulation_run)
         else:
             without_isolation.append(simulation_run)
@@ -67,21 +68,21 @@ def __draw_mask_percentage_graphs(simulation_runs: List[SimulationRun]):
     without_isolation_y: [float] = []
     for simulation_run in without_isolation:
         x.append(f"{simulation_run.get_percentage_masks() * 100}%")
-        without_isolation_y.append(simulation_run.get_fitness_score()[constants.DAYS_SINCE_OUTBREAK])
+        without_isolation_y.append(simulation_run.get_infected())
 
     pyplot.figure(mask_values)
     pyplot.plot(x, without_isolation_y, color="red", label="Without Isolation")
 
     with_isolation_y: [float] = []
     for simulation_run in with_isolation:
-        with_isolation_y.append(simulation_run.get_fitness_score()[constants.DAYS_SINCE_OUTBREAK])
+        with_isolation_y.append(simulation_run.get_infected())
 
     pyplot.figure(mask_values)
     pyplot.plot(x, with_isolation_y, color="blue", label="With Isolation")
     pyplot.legend()
     pyplot.xlabel("Mask Usage Percentage")
     pyplot.ylabel("Fitness Score")
-    pyplot.title("Mask Usage Fitness")
+    pyplot.title("Number of Infected Based on Mask Usage")
 
 
 def __draw_timestep_graphs(simulation_runs: List[SimulationRun]):
@@ -115,7 +116,6 @@ def __plot_run_fitness_timestep(simulation_run: SimulationRun, figure, counter):
 
 def __plot_benchmark_fitness_timestep(simulation_runs: List[SimulationRun], figures: List[str]) -> None:
     benchmark_run: SimulationRun = simulation_runs[0]
-    del simulation_runs[0]
 
     benchmark_x = list(benchmark_run.get_fitness_score().keys())
     benchmark_y = list(benchmark_run.get_fitness_score().values())
